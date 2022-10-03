401be0d7eeff31cebaa561256138ea52ab1edfb0
mathiasbarreto <mathiasbarreto.dev@gmail.com>
2022-10-03 03:55:01 -0300

    adds assignees

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :assignees
   devise_for :requesters
-  root "orders#index"
+  
+  get "home/index"  
+  root to: "home#index"  
 
   namespace :api do
     namespace :v1 do
