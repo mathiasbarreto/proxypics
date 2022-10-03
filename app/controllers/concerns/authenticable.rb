83a6ba678ac33c8ea768ededcf99d6cb5818bcfc
mathiasbarreto <mathiasbarreto.dev@gmail.com>
2022-10-02 22:27:48 -0300

    adds requesters

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -2,16 +2,17 @@ module Authenticable
   extend ActiveSupport::Concern
 
   included do
-    before_action :verify_api_key
+    before_action :verify_requester_api_key
   end
 
   private
 
-  def verify_api_key
-    return head :unauthorized unless has_valid_api_key?
+  def verify_requester_api_key
+    return head :unauthorized unless requester_has_valid_api_key?
   end
 
-  def has_valid_api_key?
-    Requester.find_by(apikey: request.headers['Authorization'].gsub('Bearer','').strip).present?
+  def requester_has_valid_api_key?
+    Requester.find_by(api_key: request.headers['Authorization']
+             .gsub('Bearer','').strip).present?
   end
 end
