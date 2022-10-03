289664aabf34ae0f4772618d7aae2897e34bbdb7
mathiasbarreto <mathiasbarreto.dev@gmail.com>
2022-10-03 02:11:34 -0300

    adds json return only

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Proxypics
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.api_only = true
+
 
     # Configuration for the application, engines, and railties goes here.
     #
