63485d3b717d7777813e51152bbb165e2158aa50
mathiasbarreto <mathiasbarreto.dev@gmail.com>
2022-10-02 23:29:08 -0300

    adds api_key to requesters

--- app/models/requester.rb
+++ app/models/requester.rb
@@ -1,2 +1,11 @@
 class Requester < ApplicationRecord
+  before_create :set_api_key, if: :blank_api_key?
+
+  def set_api_key
+    self.api_key = SecureRandom.hex(16)
+  end
+
+  def blank_api_key?
+    api_key.blank?
+  end
 end
